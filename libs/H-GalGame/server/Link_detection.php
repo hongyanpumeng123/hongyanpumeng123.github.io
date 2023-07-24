25fb18104924d8c9ac2fe0529b0ff77152ff6945
unknown <hongyanpumeng123>
2023-07-24 21:45:32 +0800

    Site updated: 2023-07-24 21:45:31

--- libs/H-GalGame/server/Link_detection.php
+++ libs/H-GalGame/server/Link_detection.php
@@ -1,32 +1,12 @@
 <?php
-
-function checkUrl($url) {
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_NOBODY, true);
-    curl_exec($ch);
-    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-
-    if ($httpCode == 200) {
-        return true;
+// Link_detection.php
+if (isset($_GET['url'])) {
+    $url = $_GET['url'];
+    $headers = get_headers($url, 1);
+    if (strpos($headers[0], '200') !== false) {
+        echo 'true';
     } else {
-        return false;
+        echo 'false';
     }
 }
-
-$links = array(
-    "百度网盘" => "https://pan.baidu.com/",
-    "OneDrive" => "https://onedrive.live.com/",
-    "阿里云盘" => "https://www.aliyundrive.com/",
-    "Google盘" => "https://drive.google.com/"
-);
-
-foreach ($links as $name => $url) {
-    if (checkUrl($url)) {
-        echo $name . ' 链接有效' . "\n";
-    } else {
-        echo $name . ' 链接无效' . "\n";
-    }
-}
-
 ?>
